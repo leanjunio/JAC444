abb8898a5784ca669321ad5a2d1ce31ca5942656
Lean Junio <leanjunio@live.com>
2019-05-15 09:32:41 -0400

    push

---
+++ LAB01/Output/Output.java
@@ -0,0 +1,13 @@
+/**
+ * Output
+ */
+public class Output {
+  public static void main(String[] args) {
+    for (int i = 1; i < 8; i++) {
+      for (int x = i; x <= i; x++) {
+        System.out.println(x);
+      }
+      System.out.println(i);
+    }
+  }
+}
