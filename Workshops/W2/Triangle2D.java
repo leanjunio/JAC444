fe6742d5abb50c057b47c072c378eaa9d81b5507
Lean Junio <leanjunio@live.com>
2019-05-28 18:12:29 -0400

    getPerimeter() getArea()

--- Workshops/W2/Triangle2D.java
+++ Workshops/W2/Triangle2D.java
@@ -6,12 +6,18 @@ public class Triangle2D {
   MyPoint p2;
   MyPoint p3;
 
+  /**
+   * A no-arg constructor that creates a default triangle with the points (0, 0), (1, 1), and (2, 5)
+   */
   public Triangle2D() {
     this.p1 = new MyPoint(0, 0);
     this.p1 = new MyPoint(1, 1);
     this.p1 = new MyPoint(2, 5);
   }
-  
+
+  /**
+   * A constructor that creates a triangle with the specified points
+   */
   public Triangle2D(MyPoint p1, MyPoint p2, MyPoint p3) {
     this.p1 = p1;
     this.p1 = p2;
@@ -42,23 +48,43 @@ public class Triangle2D {
     this.p3 = p3;
   }
 
+  /**
+   * Calculates the area of the current object's triangle by:
+   * 1. Get all the lengths of all the sides of the triangle
+   * 2. Calculate the semi-perimeter of the triangle using the sides that were calculated
+   * 3. Calculate the area of the triangle
+   * @return area of current object's triangle
+   */
   public double getArea() {
-    
+    double distanceP1P2 = p1.distance(p2.getX(), p2.getY());
+    double distanceP2P3 = p2.distance(p3.getX(), p3.getY());
+    double distanceP3P1 = p3.distance(p1.getX(), p1.getY());
+
+    double s = (distanceP1P2 + distanceP2P3 + distanceP3P1)/2;
+    double area = Math.sqrt(s* (s - distanceP1P2) * (s - distanceP2P3) * (s - distanceP3P1));
+    return area;
   }
 
+  /**
+   * Calculates and returns the perimeter of the current' object's triangle
+   * @return object's perimeter
+   */
   public double getPerimeter() {
-    
+    double distanceP1P2 = p1.distance(p2.getX(), p2.getY());
+    double distanceP2P3 = p2.distance(p3.getX(), p3.getY());
+    double distanceP3P1 = p3.distance(p1.getX(), p1.getY());
+    return distanceP1P2 + distanceP2P3 + distanceP3P1;
   }
 
   public boolean contains(MyPoint p) {
-    
+
   }
 
   public boolean contains(Triangle2D t) {
-    
+
   }
 
   public boolean overlaps(Triangle2D t) {
-    
+
   }
 }
