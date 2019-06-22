9eac53ce1fa5a2965ac06d1d2e813606578d24ff
Lean Junio <leanjunio@live.com>
2019-06-21 23:43:02 -0400

    upload

---
+++ Workshops/W5/Chess/chess-app/src/main/java/com/jac444w5t1/app/Main.java
@@ -0,0 +1,58 @@
+package com.jac444w5t1.app;
+
+import javafx.application.Application;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) throws Exception {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Rectangle[][] board = new Rectangle[8][8];
+		GridPane gridpane = new GridPane();
+
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				if (row % 2 == 0) {
+					if (col % 2 == 0) {
+						board[row][col] = CreateRectangle(Color.WHITE);
+					} else {
+						board[row][col] = CreateRectangle(Color.BLACK);
+					}
+				}
+				if (row % 2 != 0) {
+					if (col % 2 == 0) {
+						board[row][col] = CreateRectangle(Color.BLACK);
+					} else {
+						board[row][col] = CreateRectangle(Color.WHITE);
+					}
+				}
+			}
+		}
+
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				gridpane.add(board[row][col], col, row);
+			}
+		}
+		
+		// Create a scene and place it in the stage
+		Scene scene = new Scene(gridpane, 800, 800);
+		primaryStage.setTitle("Chess board");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public Rectangle CreateRectangle(Color c) {
+		Rectangle r = new Rectangle(100, 100, c);
+		return r;
+	}
+}
