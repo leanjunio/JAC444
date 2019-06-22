9bb3293bb0a3663102f79a2c5e11b130486c6fd9
Lean Junio <leanjunio@live.com>
2019-06-21 21:57:44 -0400

    chessboard

--- Workshops/W5/Chess/ChessBoard/src/app/App.java
+++ Workshops/W5/Chess/ChessBoard/src/app/App.java
@@ -21,110 +21,43 @@ public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		// Rectangle[] tiles = new Rectangle[64];
-		Rectangle[] tileRow = new Rectangle[8];
-
-		// Create a Rectangle and fill it with the correct Color
-		// for (int i = 0; i < 64; i++) {
-		// tiles[i] = new Rectangle(100, 100);
-		// if (i % 2 == 0) {
-		// tiles[i].setFill(Color.WHITE);
-		// } else {
-		// tiles[i].setFill(Color.BLACK);
-		// }
-		// }
-
-		// fills the tileRow with colors (8 rectangles)
-		boolean reverse = false;
-
-		// try {
-		// for (int i = 0; i < 64; i++) {
-		// boolean flippable = i % 8 == 0 && i != 0;
-		// if (flippable) {
-		// for (int x = i; x < i + 8; x++) {
-		// Rectangle temp = new Rectangle(100, 100);
-		// temp = tiles[x];
-		// tiles[x] = tiles[x+1];
-		// tiles[x+1] = temp;
-		// }
-		// i += 8;
-		// }
-		// }
-		// } catch (Exception e) {
-		// System.err.println(e.getLocalizedMessage());
-		// }
-
+		Rectangle[][] board = new Rectangle[8][8];
 		GridPane gridpane = new GridPane();
 
-		try {
-			int row = 1;
-			for (int i = 0; i < 64; i++) {
-				
-				// Creates new Rectangles
-				if (reverse) {
-					for (int x = 0; x < 8; x++) {
-						tileRow[x] = new Rectangle(100, 100);
-						if (x % 2 == 0) {
-							tileRow[x].setFill(Color.WHITE);
-						} else {
-							tileRow[x].setFill(Color.BLACK);
-						}
-					}
-					reverse = false;
-				} else {
-					for (int x = 0; x < 8; x++) {
-						tileRow[x] = new Rectangle(100, 100);
-						if (x % 2 == 0) {
-							tileRow[x].setFill(Color.BLACK);
-						} else {
-							tileRow[x].setFill(Color.WHITE);
-						}
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				if (row % 2 == 0) {
+					if (col % 2 == 0) {
+						board[row][col] = CreateRectangle(Color.WHITE);
+					} else {
+						board[row][col] = CreateRectangle(Color.BLACK);
 					}
 				}
-				
-				// adds the tileRow into the gridPane
-				for (int x = 0; x < 8; x++) {
-					gridpane.add(tileRow[x], x, row - 1, row, row);
+				if (row % 2 != 0) {
+					if (col % 2 == 0) {
+						board[row][col] = CreateRectangle(Color.BLACK);
+					} else {
+						board[row][col] = CreateRectangle(Color.WHITE);
+					}
 				}
-				i += 8;
-				row++;
-				reverse = true;
 			}
-		} catch (Exception e) {
-			System.err.println(e.getLocalizedMessage());
 		}
 
-		// gridpane.add(tiles[0], 0, 0, 1, 1);
-		// gridpane.add(tiles[1], 1, 0, 1, 1);
-		// gridpane.add(tiles[2], 2, 0, 1, 1);
-		// gridpane.add(tiles[3], 3, 0, 1, 1);
-		// gridpane.add(tiles[4], 4, 0, 1, 1);
-		// gridpane.add(tiles[5], 5, 0, 1, 1);
-		// gridpane.add(tiles[6], 6, 0, 1, 1);
-		// gridpane.add(tiles[7], 7, 0, 1, 1);
-
-		// gridpane.add(tiles[8], 0, 1, 2, 2);
-		// gridpane.add(tiles[9], 1, 1, 2, 2);
-		// gridpane.add(tiles[10], 2, 1, 2, 2);
-		// gridpane.add(tiles[11], 3, 1, 2, 2);
-		// gridpane.add(tiles[12], 4, 1, 2, 2);
-		// gridpane.add(tiles[13], 5, 1, 2, 2);
-		// gridpane.add(tiles[14], 6, 1, 2, 2);
-		// gridpane.add(tiles[15], 7, 1, 2, 2);
-
-		// gridpane.add(tiles[16], 0, 1, 3, 3);
-		// gridpane.add(tiles[17], 1, 1, 3, 3);
-		// gridpane.add(tiles[18], 2, 1, 3, 3);
-		// gridpane.add(tiles[19], 3, 1, 3, 3);
-		// gridpane.add(tiles[20], 4, 1, 3, 3);
-		// gridpane.add(tiles[21], 5, 1, 3, 3);
-		// gridpane.add(tiles[22], 6, 1, 3, 3);
-		// gridpane.add(tiles[23], 7, 1, 3, 3);
-
+		for (int row = 0; row < 8; row++) {
+			for (int col = 0; col < 8; col++) {
+				gridpane.add(board[row][col], col, row);
+			}
+		}
+		
 		// Create a scene and place it in the stage
 		Scene scene = new Scene(gridpane, 800, 800);
 		primaryStage.setTitle("Chess board");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	public Rectangle CreateRectangle(Color c) {
+		Rectangle r = new Rectangle(100, 100, c);
+		return r;
 	}
 }
